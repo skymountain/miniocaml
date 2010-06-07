55088f4c2a55238b462ef009c17d27313d65acbf
Taro Sekiyama <t-sekiym@kuis.kyoto-u.ac.jp>
2010-06-08 00:17:14 +0900

    solved: Exercise 3.2

--- main.ml
+++ main.ml
@@ -2,15 +2,23 @@ open Syntax
 open Eval
 
 let rec read_eval_print env =
-  print_string "# ";
-  flush stdout;
-  let decl = Parser.toplevel Lexer.main (Lexing.from_channel stdin) in
-  let (id, newenv, v) = eval_decl env decl in
-    Printf.printf "val %s = " id;
-    pp_val v;
-    print_newline();
-    read_eval_print newenv
-
+  let rec next_eval env =
+    print_string "# ";
+    flush stdout;
+    try 
+      let decl = Parser.toplevel Lexer.main (Lexing.from_channel stdin) in
+      eval_decl env decl
+    with
+      Parsing.Parse_error
+    | Failure _           -> print_endline "Syntax error"; next_eval env
+    | Eval.Error s        -> print_endline s; next_eval env
+  in
+  let (id, newenv, v) = next_eval env in
+  Printf.printf "val %s = " id;
+  pp_val v;
+  print_newline();
+  read_eval_print newenv
+    
 let initial_env = 
   Environment.extend "i" (IntV 1)
     (Environment.extend "v" (IntV 5) 
@@ -19,4 +27,6 @@ let initial_env =
              (Environment.extend "iii" (IntV 3)
                 (Environment.extend "iv" (IntV 4) Environment.empty)))))
     
-let _ = read_eval_print initial_env
+let _ =
+  read_eval_print initial_env
+    
