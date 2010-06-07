466a4d57ad6b762c98146d2b86b2043bca98ed19
tsekiym <tsekiym@tsekiym-pc.(none)>
2010-06-07 23:49:36 +0900

    Initial project

---
+++ eval.ml
@@ -0,0 +1,45 @@
+open Syntax 
+
+type exval = 
+    IntV of int
+  | BoolV of bool
+and dnval = exval
+
+exception Error of string
+
+let err s = raise (Error s)
+
+(* pretty printing *)
+let rec pp_val = function
+    IntV i -> 
+      print_int i
+  | BoolV b -> 
+      if b then print_string "true" else print_string "false"
+
+let rec apply_prim op arg1 arg2 = match op, arg1, arg2 with
+    Plus, IntV i1, IntV i2 -> IntV (i1 + i2)
+  | Plus, _, _ -> err ("Both arguments must be integer: +")
+  | Mult, IntV i1, IntV i2 -> IntV (i1 * i2)
+  | Mult, _, _ -> err ("Both arguments must be integer: *")
+  | Lt, IntV i1, IntV i2 -> BoolV (i1 < i2)
+  | Lt, _, _ -> err ("Both arguments must be integer: <")
+
+let rec eval_exp env = function
+    Var x -> 
+      (try Environment.lookup x env with 
+        Environment.Not_bound -> err ("Variable not bound: " ^ x))
+  | ILit i -> IntV i
+  | BLit b -> BoolV b
+  | BinOp (op, exp1, exp2) -> 
+      let arg1 = eval_exp env exp1 in
+      let arg2 = eval_exp env exp2 in
+      apply_prim op arg1 arg2
+  | IfExp (exp1, exp2, exp3) ->
+      let test = eval_exp env exp1 in
+        (match test with
+            BoolV true -> eval_exp env exp2 
+          | BoolV false -> eval_exp env exp3
+          | _ -> err ("Test expression must be boolean: if"))
+
+let eval_decl env = function
+    Exp e -> let v = eval_exp env e in ("-", env, v)
---
+++ parser.mly
@@ -0,0 +1,45 @@
+%{
+open Syntax
+%}
+
+%token LPAREN RPAREN SEMISEMI
+%token PLUS MULT LT
+%token IF THEN ELSE TRUE FALSE
+
+%token <int> INTV
+%token <Syntax.id> ID
+
+%start toplevel
+%type <Syntax.program> toplevel
+%%
+
+toplevel :
+    Expr SEMISEMI { Exp $1 }
+
+Expr :
+    IfExpr { $1 }
+  | LTExpr { $1 }
+
+LTExpr : 
+    PExpr LT PExpr { BinOp (Lt, $1, $3) }
+  | PExpr { $1 }
+
+PExpr :
+    PExpr PLUS MExpr { BinOp (Plus, $1, $3) }
+  | MExpr { $1 }
+
+MExpr : 
+    MExpr MULT AExpr { BinOp (Mult, $1, $3) }
+  | AExpr { $1 }
+
+AExpr :
+    INTV { ILit $1 }
+  | TRUE { BLit true }
+  | FALSE { BLit false }
+  | ID { Var $1 }
+  | LPAREN Expr RPAREN { $2 }
+
+IfExpr :
+    IF Expr THEN Expr ELSE Expr { IfExp ($2, $4, $6) }
+
+   
